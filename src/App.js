d729d1a970f4a71842891650be51f8d40d0dbdac
Sanskriti <sanskriti.agarwal.sa@gmail.com>
2021-09-18 15:41:16 +0530

    created Public and Private Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Switch } from 'react-router';
 import 'rsuite/dist/styles/rsuite-default.css';
+import Signin from './pages/Signin';
 import './styles/main.scss';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
 
-const App = () => <div>hello</div>;
+const App = () => {
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
+};
 
 export default App;
