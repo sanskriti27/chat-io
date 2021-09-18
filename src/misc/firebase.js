964eac27ac83e9e68f9399127877793894ea6796
Sanskriti <sanskriti.agarwal.sa@gmail.com>
2021-09-18 16:47:01 +0530

    added sign in page and facebook, google signin added

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyBOMgmD_OR4zgOvplvzOJLgyhgE98JE6ZA',
@@ -11,3 +13,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
