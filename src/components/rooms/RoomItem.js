37aad9cbbf81628b276369d54119e71f5642672b
Sanskriti <sanskriti.agarwal.sa@gmail.com>
2021-12-05 02:47:37 +0530

    Added visual presence, online offline

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -9,7 +9,7 @@ const RoomItem = ({ room }) => {
       <div className="d-flex justify-content-between align-items-center">
         <h3 className="text-disappear">{name}</h3>
         <TimeAgo
-          datetime={lastMessage ? new Date(lastMessage) : new Date(createdAt)}
+          datetime={lastMessage ? new Date(createdAt) : new Date(createdAt)}
           className="font-normal text-black-45"
         />
       </div>
